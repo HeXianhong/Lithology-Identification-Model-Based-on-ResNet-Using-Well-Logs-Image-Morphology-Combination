a63e181cc0ba116fe33f951973280fb3e820bb96
HeXianhong <142429078+HeXianhong@users.noreply.github.com>
2024-03-18 23:31:22 +0800

    Update PretainingImageGenerator.py

--- PretainingImageGenerator.py
+++ PretainingImageGenerator.py
@@ -3,9 +3,11 @@ import random
 
 
 def draw(i):
+    # Set each pic's size as 500x110
     image = Image.new('RGB', (500, 110), 'white')
     draw = ImageDraw.Draw(image)
 
+    # Set the lines' position through changing the randint parameters
     red_x = random.randint(190, 230)
     green_x = random.randint(230, 260)
     blue_x = random.randint(260, 320)
@@ -16,9 +18,10 @@ def draw(i):
 
     draw.line((blue_x, 0, blue_x, 110), fill='blue', width=3)
 
+    # Set the pics save path
     path = r"path" + 'name' + str(i) + ".jpg"
     image.save(path)
 
-
+# Through setting the loop times to generate pics
 for i in range(1, 101):
     draw(i)
